ea397c3ef01c62c0c0f9e5d99b2bda877dbeeb32
Mawnz <mans.aberg@gmail.com>
2017-05-15 14:27:12 +0200

    test with training simple network

--- app/js/test.js
+++ app/js/test.js
@@ -1,2 +1,30 @@
-//Test for da neural network lol
-//set up things required
+//activation function for later
+var activation = function(val){
+	return val/2;
+}
+
+//--Building network--
+
+//create layers
+var A = new synaptic.Layer(5);
+var B = new synaptic.Layer(3);
+
+A.project(B);
+
+//Training
+
+//Learning rate
+var learningRate = .3;
+
+for(var i = 0; i < 20000; i ++){
+	//When A activates "activation"
+	A.activate([1, 0, 2, 0, 1]);
+
+	//train B to activate 0
+	B.activate();
+	B.propagate(learningRate, [1, 5, 1]);
+}
+
+//testing
+console.log(A.activate([1, 0, 2, 0, 1]));
+console.log(B.activate());
